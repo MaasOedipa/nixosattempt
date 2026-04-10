aa335f9694a0b239c232ff03060ed87e18c57fce
MaasOedipa <MaasOedipa@github.com>
2026-04-10 14:00:14 +0200

    kitty conf added to home-manager

--- home/user/config.nix
+++ home/user/config.nix
@@ -5,5 +5,6 @@ in
 		".config/hypr".source = "${configDir}/hypr";
 		".config/wallpaper".source = "${configDir}/wallpaper";
 		".config/wofi/style.css".source = "${configDir}/wofi/wofistyle.css";
+		".config/kitty".source = "${configDir}/kitty";
 	};
 }
